28c69f20ed10b7dfa6df6f4dfabee785358714f0
Labbitzy <31593835+Labbitzy@users.noreply.github.com>
2022-07-13 11:25:55 -0400

    Time: 712 ms (24.41%), Space: 0B (100.00%) - LeetHub

---
+++ 1142-user-activity-for-the-past-30-days-ii/1142-user-activity-for-the-past-30-days-ii.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select round(ifnull(count(distinct session_id)/count(distinct user_id),0),2) as average_sessions_per_user
+from Activity
+where activity_date between date_sub('2019-07-27', interval 29 day) and '2019-07-27';
